a4b786bc0ec24f877e7eb4d8320c00bb21ece22d
kevenlwr <keven.santana@nave.org.br>
2026-07-31 13:29:18 -0300

    Implement activity loading in Atv01UmItem component
    Add functionality to fetch and display activity data from an API.

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,3 +22,51 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+import React, { useState } from "react";
+import { View, Pressable, Text } from "react-native";
+
+export default function Atv01UmItem() {
+  const [atividade, setAtividade] = useState();
+
+  async function name(params) {
+    
+  } carregarAtividade = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1"
+    ,{ method: 'GET', }
+    )
+        .then((resposta) => {
+      console.log(resposta)
+      return resposta.json()
+    })
+    .then((resultado) => {
+      console.log(resultado)
+      .catch((error) => {
+        console.error("Erro ao carregar atividade:", error);
+
+              const usuario = <View style={estilo.dados}>
+                <Text>Nome: {resultado.name}</Text>
+                <Text>Email: {resultado.email}</Text>
+                <Text>Site: {resultado.website}</Text>
+                <Text>Telefone: {resultado.phone}</Text>
+              </View>
+        
+              setResultado(usuario)
+      });
+  })};
+
+  return (
+    <View>
+      <Pressable onPress={carregarAtividade}>
+        <Text>Clique abaixo para carregar uma atividade</Text>
+      </Pressable>
+
+      {atividade  (
+        <Text>
+          {atividade.id} - {atividade.title}:{" "}
+          {atividade.completed ? "feito" : "a fazer"}
+        </Text>
+      )}
+    </View>
+  );
+}
