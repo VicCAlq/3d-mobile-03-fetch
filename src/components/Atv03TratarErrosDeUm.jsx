9718b98183b09cc609007a6f94a7a7733a7d67ee
Pensologosabo <gabriel.donato@nave.org.br>
2026-08-06 11:11:07 -0300

    Update Atv03TratarErrosDeUm.jsx

--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -27,3 +27,84 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
+
+// Estilos de "CSS"
+const estilo = StyleSheet.create({
+  usuario: {
+    backgroundColor: "#8c8",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+  dados: {
+    backgroundColor: "#6a6",
+    padding: "5px",
+    borderRadius: "5px",
+    margin: "10px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+})
+
+export default function Atv03TratarErrosDeUm() {
+
+  // Variável que vai armazenar a lista recebida
+  const [resultado, setResultado] = useState(<Text>A lista de usuários aparecerá aqui no lugar deste texto</Text>)
+
+  // Função que vai carregar a lista do endereço listado.
+  async function carregarLista() {
+    // 1ª etapa: Enviar requisição
+    await fetch(
+      'https://jsonplaceholder.typicode.com/comments/20',
+      { method: 'GET', }
+    )
+    // 2ª etapa: Receber e tratar a resposta
+    .then((resposta) => {
+      return resposta.json()
+    })
+    // 3ª etapa: Usar o resultado
+    .then((resultado) => {
+      // Aqui como se trata de uma lista de usuários, usaremos um método
+      // chamado "map", que funciona como um loop de "for item in lista"
+      
+        
+      setResultado(<View style={estilo.dados}>
+        <Text>{resultado.postId}: {resultado.id} - {resultado.email}</Text>
+        <Text>{resultado.name}</Text>
+          <Text>{resultado.body}</Text>
+        
+      </View>)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+
+    
+  }
+// Parte visual do componente
+  return(
+    <View style={estilo.usuario}>
+      <Text>
+        Carregue a lista de usuários abaixo:
+      </Text>
+      <Pressable style={estilo.botao} onPress={() => carregarLista()}>
+        <Text style={estilo.textoBotao}>Carregar usuário</Text>
+      </Pressable>
+      {resultado}
+    </View>
+  )
+}
+  
+
