fc94f7f98059aa7383551cabbd2fbc1b875e445b
netod4rk <severino.neto@nave.org.br>
2026-08-13 11:05:15 -0300

    Refactor Atv02MuitosItens component structure

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -2,7 +2,7 @@
   * ATIVIDADE 02: MUITOS ITENS
   *
   * Crie e exporte por padrão um componente chamado Atv02MuitosItens, que deve ter
-  * uma <View>, e dentro desta <View> um <Pressable> com o conteúdo
+  * uma <View>, e dentro desta <View> um <Pressable> com o conteúdo 
   * "Clique abaixo para carregar várias atividades", que quando pressionado
   * fará uma requisição usando "fetch" para a URL abaixo:
   *
@@ -20,105 +20,62 @@
   * [id] - [title]: [status]
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
-  * O valor de "status" deve ser "feito" se completed for true,
+  * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
-// Elementos necessários para o componente
-import { View, Text, Pressable, StyleSheet } from 'react-native'
-import { useState } from 'react'
 
+import React, { useState } from "react";
+import { View, Pressable, Text } from "react-native";
 
-// Estilos de "CSS"
-const estilo = StyleSheet.create({
-  usuario: {
-    backgroundColor: "#8ac",
-    padding: "10px",
-    borderRadius: "10px",
-    margin: "20px",
-  },
-  dados: {
-    backgroundColor: "#68a",
-    padding: "5px",
-    borderRadius: "5px",
-    margin: "10px",
-  },
-  textoBotao: {
-    color: "#eee",
-    fontSize: "16px",
-  },
-  botao: {
-    borderRadius: "5px",
-    backgroundColor: "#505560",
-    padding: "10px",
-    margin: "10px",
-  },
-})
+export default function Atv01UmItem() {
+  const [atividade, setAtividade] = useState();
 
-
-export default function Exemplo02Tratamento() {
-
-
-  // Variável que vai armazenar a lista recebida
-  const [resultado, setResultado] = useState(<Text>O usuário aparecerá aqui no lugar deste texto</Text>)
-
-
-  // Função que vai carregar a lista do endereço listado.
-  async function carregarUsuario() {
-    // 1ª etapa: Enviar requisição
-    // "fetch" é a função que envia uma mensagem para um endereço.
+  async function name(params) {
+    
     await fetch(
-      // Este é o endereço a ser acessado
-      'https://jsonplaceholder.typicode.com/todos',
-      // Aqui definimos o método da requisição
-      { method: 'GET', }
+      "https://jsonplaceholder.typicode.com/todos/1"
+      ,{ method: 'GET', }
     )
-    // 2ª etapa: Receber e tratar a resposta
     .then((resposta) => {
-      // Se a resposta não tiver um valor "ok", anunciamos um erro
-      if (!resposta.ok) {
-        throw new Error(`Erro na requisição! Status: ${resposta.status}`);
-      }
-      // Se não der erro, convertemos o resultado para JavaScript
+      console.log(resposta)
       return resposta.json()
     })
-    // 3ª etapa: Usar o resultado
     .then((resultado) => {
       console.log(resultado)
-      let usuario = resultado.map((item) => {
-        let feito = ""
-        if (item.completed === true){
-          feito = "feito"
-        }
-        else {
-          feito = "a fazer"
-        }
-        return <View style={estilo.dados}>
-          <Text>{item.id} - {item.title} : {feito}</Text>
-        </View>
-      })
-     
-      // Jogamos o valor da lista de itens a serem exibidos para
-      // a variável de estado "resultado"
-      setResultado(usuario)
+      console.error("Erro ao carregar atividade:", error);
+
+
+      const listaDeUsuarios = <View>
+          
+        {resultado.map((conteudo) => {
+          return <View style={estilo.dados}>
+            <Text>Nome: {conteudo.name}</Text>
+            <Text>Email: {conteudo.email}</Text>
+            <Text>Site: {conteudo.website}</Text>
+          </View>
+        })}
+        
+      </View>
+  
+      setAtividade(listaDeUsuarios)
     })
-    // Se houverem erros mais severos, estes são tratados na função de
-    // "catch" abaixo:
     .catch(error => {
       console.log("Erro: ", error)
     })
   }
-
-
-  // Parte visual do componente
-  return(
-    <View style={estilo.usuario}>
-      <Text>
-        Clique abaixo para carregar várias atividades
-      </Text>
-      <Pressable style={estilo.botao} onPress={() => carregarUsuario()}>
-        <Text style={estilo.textoBotao}>Carregar usuário</Text>
+         
+  return (
+    <View>
+      <Pressable onPress={carregarAtividade}>
+        <Text>Clique abaixo para carregar uma atividade</Text>
       </Pressable>
-      {resultado}
+
+      {atividade  (
+        <Text>
+          {atividade.id} - {atividade.title}:{" "}
+          {atividade.completed ? "feito" : "a fazer"}
+        </Text>
+      )}
     </View>
   )
-}
+};
