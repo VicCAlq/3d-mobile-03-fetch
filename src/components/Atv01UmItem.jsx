fa273b5efd8e04fb0916a27c3cbaebb9f8f31388
thiacstaa <anthoniely.costa@nave.org.br>
2026-09-01 10:57:10 -0300

    Update Atv01UmItem.jsx

--- src/components/Atv01UmItem.jsx
+++ src/components/Atv01UmItem.jsx
@@ -22,30 +22,32 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
-// Elementos necessários para o componente
 
+// Elementos necessários para o componente
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { useState } from 'react'
-
 
 // Estilos de "CSS"
 const estilo = StyleSheet.create({
   usuario: {
-    backgroundColor: "#8ac",
+    backgroundColor: "#dac",
     padding: "10px",
     borderRadius: "10px",
     margin: "20px",
   },
+
   dados: {
-    backgroundColor: "#68a",
+    backgroundColor: "#b78",
     padding: "5px",
     borderRadius: "5px",
     margin: "10px",
   },
+
   textoBotao: {
     color: "#eee",
     fontSize: "16px",
   },
+
   botao: {
     borderRadius: "5px",
     backgroundColor: "#505560",
@@ -54,67 +56,62 @@ const estilo = StyleSheet.create({
   },
 })
 
-
-export default function Atv02MuitosItens() {
-
+export default function Atv01UmItem() {
 
   // Variável que vai armazenar a lista recebida
-  const [resultado, setResultado] = useState(
-    <Text>As atividades aparecerão aqui no lugar deste texto</Text>
-  )
+  const [resultado, setResultado] = useState(<Text></Text>)
 
+  // Função que vai carregar os dados do endereço listado.
+  // Como ela depende de um resultado que não depende apenas do
+  // próprio programa, ela se trada de uma função "assíncrona"
+  async function carregarAtividade() {
 
-  // Função que vai carregar a lista do endereço listado.
-  async function carregarUsuario() {
-
-    // 1ª etapa: Enviar requisição
+    // 1ª etapa: Enviar a requisição
     // "fetch" é a função que envia uma mensagem para um endereço.
+    // O resultado de "fetch" é assíncrono: ele entrega uma "Promessa"
+    // de resultado, que quando concluída tem seu resultado processado.
     await fetch(
       // Este é o endereço a ser acessado
-      'https://jsonplaceholder.typicode.com/todos',
+      'https://jsonplaceholder.typicode.com/todos/1',
 
       // Aqui definimos o método da requisição
       { method: 'GET', }
     )
 
-    // 2ª etapa: Receber e tratar a resposta
+    // 2ª etapa: Tratar a resposta
+    // O ".then" abaixo define código a ser executado quando o "fetch"
+    // traz seu resultado. Ele traz o resultado em um formato que precisa
+    // ser convertido para código JavaScript
     .then((resposta) => {
 
-      // Se a resposta não tiver um valor "ok", anunciamos um erro
-      if (!resposta.ok) {
-        throw new Error(Erro na requisição! Status: ${resposta.status});
-      }
-
-      // Se não der erro, convertemos o resultado para JavaScript
+      // convertemos o resultado para JavaScript
       return resposta.json()
     })
 
-    // 3ª etapa: Usar o resultado
+    // 3ª etapa: Usar a resposta
+    // O próximo ".then" é onde definimos o que fazer com o resultado
+    // já processado do "fetch". Estamos aqui chamando o resultado de "resultado"
     .then((resultado) => {
 
+      // Aqui enviamos para o console do navegador.
       console.log(resultado)
 
-      let usuario = (
+      let feitoOuNao = ""
+
+      if (resultado.completed === true) {
+        feitoOuNao = "feito"
+      }
+      else {
+        feitoOuNao = "a fazer"
+      }
+
+      // O código abaixo vai organizar as informações recebidas em
+      // elementos do React Native:
+      const usuario = (
         <View style={estilo.dados}>
-
-          {resultado.map((item) => {
-
-            let feito = ""
-
-            if (item.completed === true) {
-              feito = "feito"
-            }
-            else {
-              feito = "a fazer"
-            }
-
-            return (
-              <Text key={item.id}>
-                {item.id} - {item.title}: {feito}
-              </Text>
-            )
-          })}
-
+          <Text>
+            {resultado.id} - {resultado.title}: {feitoOuNao}
+          </Text>
         </View>
       )
 
@@ -122,29 +119,22 @@ export default function Atv02MuitosItens() {
       // a variável de estado "resultado"
       setResultado(usuario)
     })
-
-    // Se houverem erros mais severos, estes são tratados na função de
-    // "catch" abaixo:
-    .catch(error => {
-      console.log("Erro: ", error)
-    })
   }
-
 
   // Parte visual do componente
   return(
     <View style={estilo.usuario}>
 
       <Text>
-        Clique abaixo para carregar várias atividades
+        Clique abaixo para carregar uma atividade:
       </Text>
 
       <Pressable
         style={estilo.botao}
-        onPress={() => carregarUsuario()}
+        onPress={() => carregarAtividade()}
       >
         <Text style={estilo.textoBotao}>
-          Carregar atividades
+          Carregar atividade
         </Text>
       </Pressable>
 
