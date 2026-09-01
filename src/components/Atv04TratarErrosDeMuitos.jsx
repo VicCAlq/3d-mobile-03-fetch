82c720f0c4f261faf0a926b46768451e8a1cb49f
thiacstaa <anthoniely.costa@nave.org.br>
2026-09-01 11:11:00 -0300

    Update Atv04TratarErrosDeMuitos.jsx

--- src/components/Atv04TratarErrosDeMuitos.jsx
+++ src/components/Atv04TratarErrosDeMuitos.jsx
@@ -60,48 +60,62 @@ const estilo = StyleSheet.create({
 })
 
 
-export default function Exemplo02Tratamento() {
+export default function Atv04TratarErrosDeMuitos() {
 
 
   // Variável que vai armazenar a lista recebida
-  const [resultado, setResultado] = useState(<Text>O usuário aparecerá aqui no lugar deste texto</Text>)
+  const [resultado, setResultado] = useState(
+    <Text>O usuário aparecerá aqui no lugar deste texto</Text>
+  )
 
 
   // Função que vai carregar a lista do endereço listado.
   async function carregarUsuario() {
+
     // 1ª etapa: Enviar requisição
     // "fetch" é a função que envia uma mensagem para um endereço.
     await fetch(
+
       // Este é o endereço a ser acessado
-      'https://jsonplaceholder.typicode.com/todos',
+      'https://jsonplaceholder.typicode.com/comments',
+
       // Aqui definimos o método da requisição
       { method: 'GET', }
     )
+
     // 2ª etapa: Receber e tratar a resposta
     .then((resposta) => {
+
       // Se a resposta não tiver um valor "ok", anunciamos um erro
       if (!resposta.ok) {
         throw new Error(`Erro na requisição! Status: ${resposta.status}`);
       }
+
       // Se não der erro, convertemos o resultado para JavaScript
       return resposta.json()
     })
+
     // 3ª etapa: Usar o resultado
     .then((resultado) => {
+
       console.log(resultado)
+
       let usuario = resultado.map((item, i) => {
- 
-        return <View style={estilo.dados} key={i}>
-          <Text>{item.id} - {item.email}</Text>
-          <Text>{item.name}</Text>
-          <Text>{item.body}</Text>
-        </View>
+
+        return (
+          <View style={estilo.dados} key={i}>
+            <Text>{item.postId}: {item.id} - {item.email}</Text>
+            <Text>{item.name}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )
       })
-     
+
       // Jogamos o valor da lista de itens a serem exibidos para
       // a variável de estado "resultado"
       setResultado(usuario)
     })
+
     // Se houverem erros mais severos, estes são tratados na função de
     // "catch" abaixo:
     .catch(error => {
@@ -113,13 +127,23 @@ export default function Exemplo02Tratamento() {
   // Parte visual do componente
   return(
     <View style={estilo.usuario}>
+
       <Text>
-        Clique abaixo para carregar várias atividades
+        Clique abaixo para carregar uma atividade
       </Text>
-      <Pressable style={estilo.botao} onPress={() => carregarUsuario()}>
-        <Text style={estilo.textoBotao}>Carregar usuário</Text>
+
+      <Pressable
+        style={estilo.botao}
+        onPress={() => carregarUsuario()}
+      >
+        <Text style={estilo.textoBotao}>
+          Carregar atividades
+        </Text>
       </Pressable>
+
       {resultado}
+
     </View>
   )
+}
 }
