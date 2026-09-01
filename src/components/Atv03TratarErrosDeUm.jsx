905b66ea359420fa85ddfd73ebc82a3d496bbb1d
SenaPaiva <mariaeduarda.sena@nave.org.br>
2026-08-31 22:43:32 -0300

    Update

--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -27,3 +27,81 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
+
+const estilo = StyleSheet.create({
+  usuario: {
+    backgroundColor: "#8ac",
+    padding: "10px",
+    borderRadius: "10px",
+    margin: "20px",
+  },
+
+  dados: {
+    backgroundColor: "#68a",
+    padding: "5px",
+    borderRadius: "5px",
+    margin: "10px",
+  },
+
+  texBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+})
+
+export default function Atv03TratarErrosDeUm() {
+    const [resultado, setResultado] = useState(<Text>O usuário aparecerá aqui no lugar deste texto</Text>)
+  async function carregarAtividade() {
+    await fetch(
+      'https://jsonplaceholder.typicode.com/comments/20',
+      { method: 'GET', }
+    )
+    .then((resposta) => {
+
+      if (!resposta.ok) {
+        throw new Error(`Erro na requisição! Status: ${resposta.status}`);}
+      return resposta.json()
+    })
+    
+    .then((resultado) => {
+      const atividade=(
+       
+        <view style={estilo.dados}>
+          <Text>{resultado.postId}: {resultado.id} - {resultado.email}</Text>
+          <Text>{resultado.name}</Text>
+          <Text>{resultado.body}</Text>
+        
+        </view>
+      )
+      setResultado(atividade) 
+    })
+    .catch((erro) => {
+    console.log("Erro: ", erro)
+    })
+  }
+ 
+  return(
+    <View>
+
+    <Pressable
+        style={estilo.botao}
+        onPress={() => carregarAtividade()}>
+        <Text style={estilo.texBotao}>
+        Carregue a atividade abaixo:
+      </Text>
+
+      </Pressable>
+      {resultado}
+    </View>
+  )
+}
