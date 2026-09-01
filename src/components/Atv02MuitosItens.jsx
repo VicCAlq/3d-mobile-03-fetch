8ce891ed0a36d63f07e8821e47da3c484ec63e93
thiacstaa <anthoniely.costa@nave.org.br>
2026-09-01 10:51:34 -0300

    Update Atv02MuitosItens.jsx

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -24,6 +24,7 @@
   * ou "a fazer" se completed for false
   */
 // Elementos necessários para o componente
+// Elementos necessários para o componente
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import { useState } from 'react'
 
@@ -55,52 +56,74 @@ const estilo = StyleSheet.create({
 })
 
 
-export default function Exemplo02Tratamento() {
+export default function Atv02MuitosItens() {
 
 
   // Variável que vai armazenar a lista recebida
-  const [resultado, setResultado] = useState(<Text>O usuário aparecerá aqui no lugar deste texto</Text>)
+  const [resultado, setResultado] = useState(
+    <Text>As atividades aparecerão aqui no lugar deste texto</Text>
+  )
 
 
   // Função que vai carregar a lista do endereço listado.
   async function carregarUsuario() {
+
     // 1ª etapa: Enviar requisição
     // "fetch" é a função que envia uma mensagem para um endereço.
     await fetch(
       // Este é o endereço a ser acessado
       'https://jsonplaceholder.typicode.com/todos',
+
       // Aqui definimos o método da requisição
       { method: 'GET', }
     )
+
     // 2ª etapa: Receber e tratar a resposta
     .then((resposta) => {
+
       // Se a resposta não tiver um valor "ok", anunciamos um erro
       if (!resposta.ok) {
-        throw new Error(`Erro na requisição! Status: ${resposta.status}`);
+        throw new Error(Erro na requisição! Status: ${resposta.status});
       }
+
       // Se não der erro, convertemos o resultado para JavaScript
       return resposta.json()
     })
+
     // 3ª etapa: Usar o resultado
     .then((resultado) => {
+
       console.log(resultado)
-      let usuario = resultado.map((item) => {
-        let feito = ""
-        if (item.completed === true){
-          feito = "feito"
-        }
-        else {
-          feito = "a fazer"
-        }
-        return <View style={estilo.dados}>
-          <Text>{item.id} - {item.title} : {feito}</Text>
+
+      let usuario = (
+        <View style={estilo.dados}>
+
+          {resultado.map((item) => {
+
+            let feito = ""
+
+            if (item.completed === true) {
+              feito = "feito"
+            }
+            else {
+              feito = "a fazer"
+            }
+
+            return (
+              <Text key={item.id}>
+                {item.id} - {item.title}: {feito}
+              </Text>
+            )
+          })}
+
         </View>
-      })
-     
+      )
+
       // Jogamos o valor da lista de itens a serem exibidos para
       // a variável de estado "resultado"
       setResultado(usuario)
     })
+
     // Se houverem erros mais severos, estes são tratados na função de
     // "catch" abaixo:
     .catch(error => {
@@ -112,13 +135,22 @@ export default function Exemplo02Tratamento() {
   // Parte visual do componente
   return(
     <View style={estilo.usuario}>
+
       <Text>
         Clique abaixo para carregar várias atividades
       </Text>
-      <Pressable style={estilo.botao} onPress={() => carregarUsuario()}>
-        <Text style={estilo.textoBotao}>Carregar usuário</Text>
+
+      <Pressable
+        style={estilo.botao}
+        onPress={() => carregarUsuario()}
+      >
+        <Text style={estilo.textoBotao}>
+          Carregar atividades
+        </Text>
       </Pressable>
+
       {resultado}
+
     </View>
   )
 }
