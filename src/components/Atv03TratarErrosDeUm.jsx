603bf9a5eb87f10475d7ac37686112b4e66611cc
mavimdddu <mariavitoria.matos@nave.org.br>
2026-09-01 11:26:30 -0300

    Update Atv03TratarErrosDeUm.jsx

--- src/components/Atv03TratarErrosDeUm.jsx
+++ src/components/Atv03TratarErrosDeUm.jsx
@@ -27,3 +27,39 @@
   * Os colchetes indicam que deve se tratar de uma variável, e não
   * do texto dentro deles.
   */
+import React, { useState } from "react";
+import { View, Pressable, Text } from "react-native";
+export default function Atv03TratarErrosDeUm() {
+  const [comentario, setComentario] = useState(null);
+  const carregarComentario = () => {
+    fetch("https://jsonplaceholder.typicode.com/comments/20")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na requisição");
+        }
+        return response.json();
+      })
+      .then((dados) => {
+        setComentario(dados);
+      })
+      .catch((erro) => {
+        console.log("Erro:", erro);
+      });
+  };
+  return (
+    <View>
+      <Pressable onPress={carregarComentario}>
+        <Text>Clique abaixo para carregar uma atividade</Text>
+      </Pressable>
+      {comentario && (
+        <View>
+          <Text>
+            {comentario.postId}: {comentario.id} - {comentario.email}
+          </Text>
+          <Text>{comentario.name}</Text>
+          <Text>{comentario.body}</Text>
+        </View>
+      )}
+    </View>
+  );
+}
