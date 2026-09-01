be7644f68b76a2517efa61c93d2b18e3d4168f7f
mavimdddu <mariavitoria.matos@nave.org.br>
2026-09-01 11:25:13 -0300

    Update Atv02MuitosItens.jsx

--- src/components/Atv02MuitosItens.jsx
+++ src/components/Atv02MuitosItens.jsx
@@ -23,3 +23,59 @@
   * O valor de "status" deve ser "feito" se completed for true, 
   * ou "a fazer" se completed for false
   */
+
+import React, { useState } from "react";
+import { View, Pressable, Text } from "react-native";
+
+export default function Atv01UmItem() {
+  const [atividade, setAtividade] = useState();
+
+  async function name(params) {
+    
+    await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+      ,{ method: 'GET', }
+    )
+    .then((resposta) => {
+      console.log(resposta)
+      return resposta.json()
+    })
+    .then((resultado) => {
+      console.log(resultado)
+      console.error("Erro ao carregar atividade:", error);
+
+
+      const listaDeUsuarios = <View>
+          
+        {resultado.map((conteudo) => {
+          return <View style={estilo.dados}>
+            <Text>Nome: {conteudo.name}</Text>
+            <Text>Email: {conteudo.email}</Text>
+            <Text>Site: {conteudo.website}</Text>
+          </View>
+        })}
+        
+      </View>
+  
+      setAtividade(listaDeUsuarios)
+    })
+    .catch(error => {
+      console.log("Erro: ", error)
+    })
+  }
+         
+  return (
+    <View>
+      <Pressable onPress={carregarAtividade}>
+        <Text>Clique abaixo para carregar uma atividade</Text>
+      </Pressable>
+
+      {atividade  (
+        <Text>
+          {atividade.id} - {atividade.title}:{" "}
+          {atividade.completed ? "feito" : "a fazer"}
+        </Text>
+      )}
+    </View>
+  )
+};
